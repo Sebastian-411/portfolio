1ba70d06925427db3586fb2b1bcd9e428f573817
Juan Sebastian Diaz <1116070867@u.icesi.edu.co>
2024-06-04 08:27:49 -0500

    chore: Optimize image loading in ExperienceCard component

--- src/app/components/card/ExperienceCard.js
+++ src/app/components/card/ExperienceCard.js
@@ -1,14 +1,13 @@
 import React from 'react'
 
-export default function ExperienceCard({onClick, experience}) {
+export default function ExperienceCard({ onClick, experience }) {
     return (
         <div>
             <div
                 onClick={() => onClick(experience)}
-                className="flex items-center justify-center aspect-square shadow-xl dark:shadow-thick hover:bg-zinc-100 dark:hover:bg-primary ring-1 bg-zinc-50 dark:ring-white/10 ring-primary/5 dark:bg-tertiary rounded-lg hover:ring-primary/5 dark:hover:ring-white/20 duration-300 group transition-all "
+                className="flex items-center  justify-center aspect-square shadow-xl dark:shadow-thick hover:bg-zinc-100 dark:hover:bg-primary ring-1 bg-zinc-50 dark:ring-white/10 ring-primary/5 dark:bg-tertiary rounded-lg hover:ring-primary/5 dark:hover:ring-white/20 duration-300 group transition-all "
             >
-                <img src={`/experiences/${experience.title}.png`} alt={experience.title} className='group-hover:scale-125 hover:rounded-lg duration-300 text-primary dark:text-white group-hover text-primary:dark:text-white/50 w-full h-full group-hover:border-radius-2' />
-            </div>
+                <img src={`/experiences/${experience.title}.png`} alt={experience.title} className='group-hover:scale-125 rounded-lg hover:rounded-lg duration-300 text-primary dark:text-white group-hover text-primary:dark:text-white/50 w-full h-full group-hover:border-radius-2 hover:border-gray-300 hover:border-2' />            </div>
 
         </div>
     )
