f735bb1c73b55c8e13eed3951960f0ca6758529b
Juan Sebastian Diaz <1116070867@u.icesi.edu.co>
2024-06-04 05:41:21 -0500

    chore: Refactor FindMe component and optimize icons

--- src/app/components/FindMe.js
+++ src/app/components/FindMe.js
@@ -11,38 +11,59 @@ export default function FindMe() {
 
     return (
         <div>
-            <div className="w-full">
-                <p dangerouslySetInnerHTML={{ __html: t("banner.find_me") }}
+            <div 
+                className="w-full"
+            >
+                <p 
+                    dangerouslySetInnerHTML={{ __html: t("banner.find_me") }}
                     className="text-xl font-normal tracking-tight text-primary dark:text-white lg:text-8xl"
                 >
 
                 </p>
             </div>
-            <div className="grid mt-4 grid-cols-4 md:grid-cols-8 lg:grid-cols-4 gap-y-4 gap-4 justify-center w-full">
+            <div 
+                className="grid mt-4 grid-cols-4 md:grid-cols-8 lg:grid-cols-4 gap-y-4 gap-4 justify-center w-full"
+            >
                 <a
                     href="mailto:sg5043161@gmail.com"
                     className="flex items-center justify-center aspect-square shadow-xl dark:shadow-thick hover:bg-zinc-100 dark:hover:bg-primary ring-1 bg-zinc-50 dark:ring-white/10 ring-primary/5 dark:bg-tertiary rounded-lg hover:ring-primary/5 dark:hover:ring-white/20 duration-300 group transition-all h-12 w-12"
                 >
-                    <CiMail className='group-hover:scale-125 duration-300 text-primary dark:text-white group-hover text-primary:dark:text-white/50' strokeWidth={0.2} size={30} style={{opacity: 0.6}}/>
+                    <CiMail 
+                        className='group-hover:scale-125 duration-300 text-primary dark:text-white group-hover text-primary:dark:text-white/50' 
+                        strokeWidth={0.2} 
+                        size={30} 
+                        style={{opacity: 0.6}}/>
                 </a>
                 <a
                     href="https://github.com/Sebastian-411"
                     className="flex items-center justify-center aspect-square shadow-xl dark:shadow-thick hover:bg-zinc-100 dark:hover:bg-primary ring-1 bg-zinc-50 dark:ring-white/10 ring-primary/5 dark:bg-tertiary rounded-lg hover:ring-primary/5 dark:hover:ring-white/20 duration-300 group transition-all h-12 w-12"
                 >
-                    <FiGithub className='group-hover:scale-125 duration-300 text-primary dark:text-white group-hover text-primary:dark:text-white/50' size={25} style={{opacity: 0.4}}/>
+                    <FiGithub 
+                        className='group-hover:scale-125 duration-300 text-primary dark:text-white group-hover text-primary:dark:text-white/50' 
+                        size={25} 
+                        style={{opacity: 0.4}}/>
                 </a>
                 <a
                     href="https://twitter.com/Sebastian411_"
                     className="flex items-center justify-center aspect-square shadow-xl dark:shadow-thick hover:bg-zinc-100 dark:hover:bg-primary ring-1 bg-zinc-50 dark:ring-white/10 ring-primary/5 dark:bg-tertiary rounded-lg hover:ring-primary/5 dark:hover:ring-white/20 duration-300 group transition-all h-12 w-12"
                 >
-                    <BsTwitterX  className='group-hover:scale-125 duration-300 text-primary dark:text-white group-hover text-primary:dark:text-white/50' strokeWidth={0.1} size={25} style={{opacity: 0.4}}/>
+                    <BsTwitterX  
+                        className='group-hover:scale-125 duration-300 text-primary dark:text-white group-hover text-primary:dark:text-white/50' 
+                        strokeWidth={0.1} 
+                        size={25} 
+                        style={{opacity: 0.4}}
+                    />
                 </a>
                 <a
                     href="https://www.linkedin.com/in/sebastian-diaz-sanchez/"
                     className="flex items-center justify-center aspect-square shadow-xl dark:shadow-thick hover:bg-zinc-100 dark:hover:bg-primary ring-1 bg-zinc-50 dark:ring-white/10 ring-primary/5 dark:bg-tertiary rounded-lg hover:ring-primary/5 dark:hover:ring-white/20 duration-300 group transition-all h-12 w-12"
                 >
-                    <CiLinkedin className='group-hover:scale-125 duration-300 text-primary dark:text-white group-hover text-primary:dark:text-white/50' strokeWidth={0.1} size={30} style={{opacity: 0.4}}/>
-
+                    <CiLinkedin 
+                        className='group-hover:scale-125 duration-300 text-primary dark:text-white group-hover text-primary:dark:text-white/50' 
+                        strokeWidth={0.1} 
+                        size={30} 
+                        style={{opacity: 0.4}}
+                    />
                 </a>
             </div>
 
