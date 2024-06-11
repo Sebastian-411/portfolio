4a72f7e344ae860c465b5cf0bf0cee1967d0234f
Sebastian._.411._ <84807030+Debug41@users.noreply.github.com>
2024-06-11 00:15:01 -0500

    refactor: Update SkillSection component to fix border color and height of list items

--- src/app/components/skills/SkillSection.js
+++ src/app/components/skills/SkillSection.js
@@ -12,10 +12,10 @@ export default function SkillSection({ scope, back, next }) {
         {scope.content.map((item, index) => (
           <li
             key={index}
-            className="flex items-center space-x-4 bg-white dark:bg-gray-800 rounded-lg shadow-md transition-transform transform hover:scale-105 border border-gray-300 "
+            className="flex items-center space-x-4 bg-white dark:bg-gray-800 rounded-lg shadow-md transition-transform transform hover:scale-105 border border-gray-300 h-56"
           >
-            <div className="flex-shrink-0 h-full flex items-center border-r border-blue-500 mt-4 mb-4 ml-4 pr-2">
-              <div className="bg-gray-500 text-white text-xl flex items-center justify-center rounded-full h-9 w-9">
+            <div className="flex-shrink-0 h-full flex items-center border-r border-gray-300 mt-4 mb-4 ml-4 pr-2">
+              <div className="bg-gray-900/80 text-white text-xl flex items-center justify-center rounded-full h-9 w-9">
                 <span className="emoji">{item.icon}</span>
               </div>
             </div>
