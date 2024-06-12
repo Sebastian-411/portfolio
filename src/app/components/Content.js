9b528e352a2ea2e972247a145275b91579b2900c
Sebastian._.411._ <84807030+Debug41@users.noreply.github.com>
2024-06-12 12:28:04 -0500

    feat: Add Project component to Content.js

--- src/app/components/Content.js
+++ src/app/components/Content.js
@@ -4,6 +4,7 @@ import CV from "./content/CV";
 import FindMe from "./content/FindMe";
 import Experience from "./content/Experience";
 import Education from "./content/Education";
+import Project from "./content/Project";
 
 export default function Content() {
   return (
@@ -22,8 +23,9 @@ export default function Content() {
           <div className="ring-1 dark:ring-white/10  ring-primary/5 flex flex-col justify-between items-center rounded-3xl shadow-xl dark:shadow-thick p-8  bg-white dark:bg-secondary overflow-hidden text-center lg:text-left">
             <CV />
           </div>
-          <div className="ring-1 lg:row-start-3 items-center h-full flex p-8 flex-col justify-center hover:ring-primary/5 dark:hover:ring-white/20 dark:ring-white/10 ring-primary/5 relative rounded-3xl overflow-hidden bg-white dark:bg-secondary shadow-xl dark:shadow-thick"></div>
-
+          <div className="ring-1 lg:row-start-3 items-center h-full flex p-8 flex-col justify-center hover:ring-primary/5 dark:hover:ring-white/20 dark:ring-white/10 ring-primary/5 relative rounded-3xl overflow-hidden bg-white dark:bg-secondary shadow-xl dark:shadow-thick">
+            <Project />
+          </div>
           <div className="lg:row-span-2 ring-1 dark:ring-white/10 ring-primary/5 hover:ring-primary/5 bg-white dark:bg-secondary  dark:hover:ring-white/20 overflow-hidden duration-300 shadow-xl dark:shadow-thick rounded-3xl p-8">
             <Education />
           </div>
