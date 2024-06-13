0b76b4c3d0cbe308d208cfca8366594de556ad6b
Sebastian._.411._ <84807030+Debug41@users.noreply.github.com>
2024-06-13 18:42:45 -0500

    refactor: Update file paths in Content.js for better organization

--- src/app/components/Content.js
+++ src/app/components/Content.js
@@ -59,33 +59,7 @@ export default function Content() {
               </form>
             </div>
           </div>
-          <div className="ring-1 dark:ring-white/10 ring-primary/5 shadow-xl dark:shadow-thick rounded-3xl p-8 lg:row-start-4 h-full flex flex-col justify-between bg-white dark:bg-secondary">
-            <p className="text-xl tracking-tight font-medium text-primary dark:text-white md:text-5xl">
-              Testimonials
-            </p>
-            <div className="md:grid md:grid-cols-2 gap-6 lg:grid-cols-1">
-              <div className="mt-4">
-                <p className="text-sm text-pink-500 dark:text-pink-400">
-                  "Big fan of your themes. They're well-organized, look clean,
-                  and are fast."
-                </p>
-                <p className="text-xs mt-2 text-zinc-500">
-                  <span className="block text-xs">Kevin Focke</span>
-                  <span className="block text-xs"> Developer</span>
-                </p>
-              </div>
-              <div className="mt-4">
-                <p className="text-sm text-orange-500 dark:text-orange-300">
-                  "Amazing resource using @astrodotbuild and @tailwindcss! I got
-                  it a week ago and have found the contents really useful. 5/5
-                </p>
-                <p className="text-xs mt-2 text-zinc-500">
-                  <span className="block text-xs">Alonso</span>
-                  <span className="block text-xs"> Entrepreneur</span>
-                </p>
-              </div>
-            </div>
-          </div>
+          <div className="ring-1 dark:ring-white/10 ring-primary/5 shadow-xl dark:shadow-thick rounded-3xl p-8 lg:row-start-4 h-full flex flex-col justify-between bg-white dark:bg-secondary"></div>
           <div className="lg:col-span-2 xl:col-span-1 h-full flex flex-col justify-between lg:row-span-2 ring-1 dark:ring-white/10 ring-primary/5 bg-white dark:bg-secondary shadow-xl dark:shadow-thick rounded-3xl p-8">
             <Carousel />
           </div>
